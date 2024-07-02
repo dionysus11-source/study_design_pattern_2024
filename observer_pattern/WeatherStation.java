dbbd65e3e53e7fa9f59a4129b6b826f51d5a84a4
dionysus11 <52480056+dionysus11-source@users.noreply.github.com>
2024-07-02 13:52:57 +0900

    remove unused character

--- observer_pattern/WeatherStation.java
+++ observer_pattern/WeatherStation.java
@@ -3,7 +3,6 @@ package observer_pattern;
 public class WeatherStation {
     public static void main(String[] args){
         WeatherData weatherData = new WeatherData();
-;       
         CurrentConditionsDisplay currentDisplay = new CurrentConditionsDisplay(weatherData);
         StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
         ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
