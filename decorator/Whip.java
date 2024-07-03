049175835b3ac567c771dfcc14ba8ef9a470509f
dionysus11 <52480056+dionysus11-source@users.noreply.github.com>
2024-07-03 15:32:10 +0900

    Add Decorator pattern test code

---
+++ decorator/Whip.java
@@ -0,0 +1,18 @@
+package decorator;
+
+public class Whip extends CondimentDecorator {
+    public Whip(Beverage beverage){
+        this.beverage = beverage;
+    }
+
+    @Override
+    public String getDescription() {
+        return beverage.getDescription() + ", 휘핑크림";
+    }
+
+    @Override
+    public double cost() {
+        return beverage.cost() + .10;
+    }
+    
+}
