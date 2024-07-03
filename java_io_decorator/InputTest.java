f30ee2f15a9108eecf26b949c63433e6ae3b6af5
dionysus11 <52480056+dionysus11-source@users.noreply.github.com>
2024-07-03 16:34:49 +0900

    io decorator

---
+++ java_io_decorator/InputTest.java
@@ -0,0 +1,23 @@
+package java_io_decorator;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class InputTest {
+    public static void main(String[] args) throws IOException{
+        int c;
+        try{
+            InputStream in = new LowerCaseInputStream(new BufferedInputStream(new FileInputStream("C:\\Users\\diony\\Documents\\study_design_pattern_2024\\java_io_decorator\\test.txt")));
+            while ((c = in.read()) >= 0){
+                System.out.print((char)c);
+            }
+
+            in.close();
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+    
+}
