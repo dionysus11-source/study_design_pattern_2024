f061ed7f23b0c78294edca9681bd04656cb72838
dionysus11 <52480056+dionysus11-source@users.noreply.github.com>
2024-07-03 15:56:31 +0900

    Fix Issue
    beverage2 -> beverage3
    
    Add size option
    사이즈 마다 첨가물의 가격이 달라짐

--- decorator/Soy.java
+++ decorator/Soy.java
@@ -10,7 +10,15 @@ public class Soy extends CondimentDecorator {
         return beverage.getDescription() + ", 두유";
     }
     public double cost(){
-        return beverage.cost() + .15;
+        double cost = beverage.cost();
+        if (beverage.getSize() == Size.TALL){
+            cost += .10;
+        }else if(beverage.getSize() == Size.GRANDE) {
+            cost += .15;
+        } else if (beverage.getSize() == Size.VENTI) {
+            cost += .20;
+        }
+        return cost;
     }
 
 
--- decorator/StarbuzzCoffee.java
+++ decorator/StarbuzzCoffee.java
@@ -1,5 +1,7 @@
 package decorator;
 
+import decorator.Beverage.Size;
+
 public class StarbuzzCoffee {
     public static void main(String[] args){
         Beverage beverage = new Espresso();
@@ -12,9 +14,10 @@ public class StarbuzzCoffee {
         System.out.println(beverage2.getDescription() + " $" + beverage2.cost());
 
         Beverage beverage3 = new HouseBlend();
-        beverage2 = new Soy(beverage2);
-        beverage2 = new Mocha(beverage2);
-        beverage2 = new Whip(beverage2);
+        beverage3.setSize(Size.GRANDE);
+        beverage3 = new Soy(beverage3);
+        beverage3 = new Mocha(beverage3);
+        beverage3 = new Whip(beverage3);
         System.out.println(beverage3.getDescription() + " $" + beverage3.cost());
 
 
