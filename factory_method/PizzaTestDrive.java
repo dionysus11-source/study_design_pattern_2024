9ebc635cf2d0d21165d53c7c9ac118c80677b0dc
dionysus11 <52480056+dionysus11-source@users.noreply.github.com>
2024-07-07 20:24:31 +0900

    first factory method

---
+++ factory_method/PizzaTestDrive.java
@@ -0,0 +1,16 @@
+package factory_method;
+
+public class PizzaTestDrive {
+    public static void main(String[] args) {
+        PizzaStore nyStore = new NYPizzaStore();
+        PizzaStore chicagoStore = new ChicagoPizzaStore();
+
+        Pizza pizza = nyStore.orderPizza("cheese");
+        System.out.println("에단이 주문한 : " + pizza.getName());
+        
+        pizza = chicagoStore.orderPizza("cheese");
+        System.out.println("조엘이 주문한 : " + pizza.getName());
+        
+    }
+    
+}
