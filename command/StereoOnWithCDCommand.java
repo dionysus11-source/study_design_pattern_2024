ea4447061253caea0db0eeb4ade3c1ce6c0632df
dionysus11 <52480056+dionysus11-source@users.noreply.github.com>
2024-07-08 22:25:25 +0900

    첫번째 리모컨 구현

---
+++ command/StereoOnWithCDCommand.java
@@ -0,0 +1,17 @@
+package command;
+
+public class StereoOnWithCDCommand implements Command{
+    Stereo stereo;
+    
+    public StereoOnWithCDCommand(Stereo stereo){
+        this.stereo = stereo;
+    }
+
+    @Override
+    public void excute() {
+        stereo.on();
+        stereo.setCD();
+        stereo.setVolume(11);
+    }
+    
+}
