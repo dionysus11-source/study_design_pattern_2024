4a590b18b1b26026042c00b711e60bb976318a60
dionysus11 <52480056+dionysus11-source@users.noreply.github.com>
2024-07-08 09:24:09 +0900

    enum을 사용하여 싱글톤 구현

--- singleton/ChocolateController.java
+++ singleton/ChocolateController.java
@@ -2,12 +2,12 @@ package singleton;
 
 public class ChocolateController {
 	public static void main(String args[]) {
-		ChocolateBoiler boiler = ChocolateBoiler.getInstance();
+		ChocolateBoiler boiler = ChocolateBoiler.UNIQUE_INSTANCE;
 		boiler.fill();
 		boiler.boil();
 		boiler.drain();
 
 		// will return the existing instance
-		ChocolateBoiler boiler2 = ChocolateBoiler.getInstance();
+		ChocolateBoiler boiler2 = ChocolateBoiler.UNIQUE_INSTANCE;
 	}
 }
