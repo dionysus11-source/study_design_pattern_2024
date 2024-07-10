1fa0ba2493b6e2bbf07198deeb31eb834f994d1c
dionysus11 <52480056+dionysus11-source@users.noreply.github.com>
2024-07-10 11:11:41 +0900

    Iterator 어댑터

---
+++ adaptor/arraylist/ArrayListAdapter.java
@@ -0,0 +1,24 @@
+package adaptor.arraylist;
+
+import java.util.Enumeration;
+import java.util.Iterator;
+
+
+public class ArrayListAdapter implements Enumeration<Object>{
+    Iterator<?> iterator;
+
+    public ArrayListAdapter(Iterator<?> iterator) {
+        this.iterator = iterator;
+    }
+
+    @Override
+    public boolean hasMoreElements() {
+        return iterator.hasNext();
+    }
+
+    @Override
+    public Object nextElement() {
+        return iterator.next();
+    }
+    
+}
